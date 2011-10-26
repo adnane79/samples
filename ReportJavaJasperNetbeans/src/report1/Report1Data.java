4b50f11208c8b7360cff542d1014706c32a166ec
radityo <radityo.p.w@gmail.com>
2011-10-26 17:50:40 +0700

    add report java jasper netbeans project

---
+++ ReportJavaJasperNetbeans/src/report1/Report1Data.java
@@ -0,0 +1,66 @@
+/**
+ * @author radityo.p.w (radityo.p.w@gmail.com)
+ * @version 1.0
+ */
+
+package report1;
+
+/**
+ *
+ * @author radityo
+ */
+public class Report1Data {
+
+    private String no;
+
+    private String Nama;
+
+    private Double harga;
+
+    private int jumlah;
+
+    private Double total;
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public Double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(Double harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+
+
+}
